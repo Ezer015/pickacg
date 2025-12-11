0930c53386793bc6bdff4f64f9679e6fd42cab0c
Ezer015 <1379242357@qq.com>
2025-12-12 01:20:05 +0800

    feat: integrate Noto Sans SC font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono, Noto_Sans_SC } from "next/font/google"
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import { Analytics } from '@vercel/analytics/next';
 import { ThemeProvider } from "@/components/theme-provider"
@@ -16,6 +16,12 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
+const notoSansSC = Noto_Sans_SC({
+    variable: "--font-noto-sans-sc",
+    subsets: ["latin"],
+    display: "swap",
+});
+
 export const metadata: Metadata = {
     title: "Pick Anime Cool",
     description: "Provide a better bangumi search experience",
@@ -28,7 +34,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <body className={`${geistSans.variable} ${geistMono.variable} ${notoSansSC.variable} antialiased`}>
                 <NuqsAdapter>
                     <ThemeProvider
                         attribute="class"
